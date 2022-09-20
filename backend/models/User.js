0e8ae6eee43a78524e570a83e7ebec96906212ca
Praik <pratikkumar791@gmail.com>
2022-09-20 21:56:51 +0530

    before error message

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
   email:{
     type: String,
     required: true,   
-    // unique: true, 
+    unique: true, 
   },
   password:{
     type: String,
@@ -21,4 +21,6 @@ const UserSchema = new Schema({
   }
 });
 
-module.exports = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
+User.createIndexes();
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const { eventWrapper } = require('@testing-library/user-event/dist/utils');
 
 
 // Create a User using: POST "/api/auth" . Doesnt require authentication
-router.post('/', (req, res)=>{
-   console.log(req.body);
-   const user = User(req.body);
-   user.save();
-   res.send(req.body);
+router.post('/',[
+    body('name','Enter a valid Name').isLength({ min: 3 }),
+    body('email', ' Enter a Valid email').isEmail(),
+    body('password', 'password should be min of 5 chars').isLength({ min: 5 }),
+], (req, res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json({user, message: "message"}))
+      res.json({error: 'The Email Value should be unique'});
 })
 
 module.exports = router;
